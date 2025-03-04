3afef2b17fed5eb3d95b80f2d721ca758f53b1c2
unknown <shalomsibi16@gmail.com>
2025-03-04 10:27:32 +0530

    saved

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Sibi Shalom </h1>
+      <p>Contact:123456789 </p>
+    </div>
+  );
+}
+export default App;
